8d9bb5bb3070ba1c01238980be5f1f64025c368f
FinoanaRandria <fitiavanarex4@gmail.com>
2023-06-19 21:29:05 +0300

    connect mongodb

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const session = require('express-session')
+const mongoose= require('mongoose')
+const app = express()
+const port =3000
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/session', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Mongodb is connected!');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
+});
+
+app.use(session({
+  secret: 'key',
+  resave: false,
+  saveUninitialized:false
+}))
+
+
+app.get('/', (req,res)=>{
+  console.log(req.session);
+  res.send('hello')
+})
+
+
+
+app.listen(port, ()=>{
+     console.log(`Server is runing on ${port}`);
+})
