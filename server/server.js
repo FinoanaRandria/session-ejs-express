2e967141f16fd47e0ce47460686aac479ff6042b
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-06-21 19:17:22 +0200

    connecte session

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const session = require('express-session')
 const mongoose= require('mongoose')
+const MongoDBSession= require('connect-mongodb-session')(session);
 const app = express()
 const port =3000
 
